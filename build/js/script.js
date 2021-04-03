a6cdf5eefa0721445301f9993e574d6f6ba6cd6c
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-04-03 21:27:24 +0500

    save

--- build/js/script.js
+++ build/js/script.js
@@ -10,7 +10,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
 document.addEventListener("DOMContentLoaded", function (event) {
   var btnToggle = document.querySelector(".page-nav__toggle");
   var pageNavList = document.querySelector(".page-nav__list");
+  var pageNavItems = document.querySelectorAll(".page-nav__item a");
   if (pageNavList) btnToggle.addEventListener("click", function () {
-    pageNavList.classList.toggle("page-nav__list--active") ? btnToggle.attributes["aria-label"].value = "Закрыть меню" : btnToggle.attributes["aria-label"].value = "Открыть меню";
+    if (pageNavList.classList.toggle("page-nav__list--active")) {
+      btnToggle.attributes["aria-label"].value = "Закрыть меню";
+      pageNavItems.forEach(function (el) {
+        return el.tabIndex = 0;
+      });
+    } else {
+      btnToggle.attributes["aria-label"].value = "Открыть меню";
+      pageNavItems.forEach(function (el) {
+        return el.tabIndex = -1;
+      });
+    }
   });
 });
