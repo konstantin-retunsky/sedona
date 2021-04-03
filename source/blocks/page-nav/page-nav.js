cac49497091f4bca3439b4df1d25dc35c140a318
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-04-03 20:29:51 +0500

    fix select index

--- source/blocks/page-nav/page-nav.js
+++ source/blocks/page-nav/page-nav.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 	let btnToggle = document.querySelector(".page-nav__toggle")
 	let pageNavList = document.querySelector(".page-nav__list")
+	let pageNavItems = document.querySelectorAll(".page-nav__item a")
 
 	if (pageNavList)
 		btnToggle.addEventListener("click", () => {
-			pageNavList.classList.toggle("page-nav__list--active")
-				? (btnToggle.attributes["aria-label"].value = "Закрыть меню")
-				: (btnToggle.attributes["aria-label"].value = "Открыть меню")
+			if (pageNavList.classList.toggle("page-nav__list--active")) {
+				btnToggle.attributes["aria-label"].value = "Закрыть меню"
+				pageNavItems.forEach((el) => (el.tabIndex = 0))
+			} else {
+				btnToggle.attributes["aria-label"].value = "Открыть меню"
+				pageNavItems.forEach((el) => (el.tabIndex = -1))
+			}
 		})
 })
