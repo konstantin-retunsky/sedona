80e947daa064b2d1272bc9266fdec941190285cf
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-04-03 10:47:46 +0500

    fix accessibility

--- source/blocks/page-nav/page-nav.js
+++ source/blocks/page-nav/page-nav.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-	let btnToggle = document.querySelector(".page-nav__toggle");
-	let pageNavList = document.querySelector(".page-nav__list");
+	let btnToggle = document.querySelector(".page-nav__toggle")
+	let pageNavList = document.querySelector(".page-nav__list")
 
 	if (pageNavList)
-		btnToggle.addEventListener("click", () =>
+		btnToggle.addEventListener("click", () => {
 			pageNavList.classList.toggle("page-nav__list--active")
-		);
-});
+				? (btnToggle.attributes["aria-label"].value = "Закрыть меню")
+				: (btnToggle.attributes["aria-label"].value = "Открыть меню")
+		})
+})
