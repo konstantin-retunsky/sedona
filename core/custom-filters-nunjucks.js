71ecc02ee5e57538fb6e423574d3a0347889c115
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-04-05 20:08:04 +0500

    add new mixin and done html feedback-form

--- core/custom-filters-nunjucks.js
+++ core/custom-filters-nunjucks.js
@@ -1,10 +1,28 @@
-module.exports = function(environment) {
 
-  environment.addFilter('getLastElement', (arr) => arr[arr.length - 1])
 
-  // environment.addFilter('slug', function(str) {
-  //   return str && str.replace(/\s/g, '-', str).toLowerCase();
-  // });
- 
-  // environment.addGlobal('globalTitle', 'My global title')
+
+
+module.exports = function (environment) {
+	environment.addFilter("getLastElement", (arr) => arr[arr.length - 1])
+
+	environment.addFilter("setAttribute", (arr) => {
+		const singleTags = ["checked", "required", "disabled"]
+
+		return arr[0] + "='" + arr[1] + "'"
+		// if (arr[1] === "" || arr[1] === undefined) {
+		// 	return ""
+		// } else if (singleTags.includes(arr[1])) {
+		// 	return arr[1]
+		// } else {
+		// 	return arr[1].length > 1
+		// 		? `${arr[0]}=${arr[1].join(" ")}`
+		// 		: `${arr[0]}=${arr[1]}`
+		// }
+	})
+
+	// environment.addFilter('slug', function(str) {
+	//   return str && str.replace(/\s/g, '-', str).toLowerCase();
+	// });
+
+	// environment.addGlobal('globalTitle', 'My global title')
 }
